67839c2a94498092359b5ce8797d068432379e5c
Madhavi-2403 <mgrandhi85@gmail.com>
2025-09-21 17:15:36 +0530

    login error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar"
 import Home from "./Home"
 //import HomePage from "./HomePage"
 import SignUp from "./SignUp"
-import Login from "./Login"
+import Login from "./login";
 import { Routes, Route } from 'react-router-dom'
 function App() {
 
